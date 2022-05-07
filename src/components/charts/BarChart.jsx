faa75a96f689362f6c02713315fd16c4ef9085c1
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-07 18:11:12 +0200

    working dynamic svg chart template

--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -1,9 +1,11 @@
 import { React, useEffect } from "react";
 import PropTypes from "prop-types";
 import * as d3 from "d3";
+import "./BarChart.css";
 
 const BarChart = ({ payload }) => {
 
+	//#:Create bar Chart object
 	const parent = d3.select("#chart-bar");
 	const yScale = d3.scaleLinear();
 	const xScale = d3.scaleLinear();
@@ -13,7 +15,11 @@ const BarChart = ({ payload }) => {
 		parent.selectAll("*").remove();
 
 		yScale.domain([d3.min(payload, d => d.calories), d3.max(payload, d => d.calories)]);
-		yScale.range([39, 1]);
+		yScale.range([5, 50]);
+
+		const ykScale = d3.scaleLinear()
+			.domain([d3.min(payload, d => d.kilogram), d3.max(payload, d => d.kilogram)])
+			.range([5, 50]);
 
 		xScale.domain([0, payload.length]);
 		xScale.range([5, 295]);
@@ -28,11 +34,21 @@ const BarChart = ({ payload }) => {
 			.data(payload)
 			.enter()
 			.append("rect")
+			.attr("x", (d, i) => `${(xScale(i) + (100 / payload.length) / 2)}`)
+			.attr("y", d => `${100 - yScale(d.calories)}`)
+			.attr("width", `${(100 / payload.length)/2}`)
+			.attr("height", (d) => yScale(d.calories))
+			.attr("class", "clr-primary")
+			.text(d => `${d.calories}`)
+			.exit()
+			.data(payload)
+			.enter()
+			.append("rect")
 			.attr("x", (d, i) => `${xScale(i)}`)
-			.attr("y", "39")
-			.attr("width", `${(100 / payload.length) - 3}px`)
-			.attr("height", (d) => yScale(d.calories) + "px")
-			.style("fill", "blue")
+			.attr("y", d => `${100 - ykScale(d.kilogram)}`)
+			.attr("width", `${(100 / payload.length)/2}`)
+			.attr("height", (d) => ykScale(d.kilogram))
+			.attr("fill", "black")
 			.text(d => `${d.calories}`);
 	}, [payload]);
 
