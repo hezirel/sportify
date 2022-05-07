d734f17e35c417d9d00902e8e33123f8ff9ab14f
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-07 17:01:52 +0200

    adjusted svg placement to dynamic sizing

--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -4,37 +4,35 @@ import * as d3 from "d3";
 
 const BarChart = ({ payload }) => {
 
-	const svg = d3.select("#chart-bar");
+	const parent = d3.select("#chart-bar");
 	const yScale = d3.scaleLinear();
 	const xScale = d3.scaleLinear();
 
 
 	useEffect(() => {
-		svg.selectAll("*").remove();
+		parent.selectAll("*").remove();
 
 		yScale.domain([d3.min(payload, d => d.calories), d3.max(payload, d => d.calories)]);
-
-		yScale.range([200, 0]);
+		yScale.range([39, 1]);
 
 		xScale.domain([0, payload.length]);
-		xScale.range([0, 300]);
+		xScale.range([5, 295]);
 
-		svg.append("svg")
-			.attr("width", "100%")
-			.attr("height", "100%")
-			.attr("id", "chart-bar-svg")
-			.style("background-color", "grey");
+		parent.append("svg")
+			.attr("viewBox", "0, 0, 300, 100")
+			.attr("preserveAspectRatio", "xMidYMid meet")
+			.style("background-color", "grey")
+			.style("border", "2px solid red");
 		
-		d3.select("svg").selectAll("rect")
+		parent.select("svg").selectAll("rect")
 			.data(payload)
 			.enter()
 			.append("rect")
-			.attr("x", (d, i) => xScale(i))
-			.attr("y", "200")
-			.attr("width", "20px")
+			.attr("x", (d, i) => `${xScale(i)}`)
+			.attr("y", "39")
+			.attr("width", `${(100 / payload.length) - 3}px`)
 			.attr("height", (d) => yScale(d.calories) + "px")
 			.style("fill", "blue")
-			.append("text")
 			.text(d => `${d.calories}`);
 	}, [payload]);
 
