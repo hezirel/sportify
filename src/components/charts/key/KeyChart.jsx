f637816fb540fb36cea9dda08fa9f30314232394
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-15 01:29:54 +0200

    KeyData working middleware

--- src/components/charts/key/KeyChart.jsx
+++ src/components/charts/key/KeyChart.jsx
@@ -9,9 +9,60 @@ import useFetch from "../../../js/useFetch";
 import Loader from "../Loader";
 import "./KeyChart.css";
 
+const UNITS = [{
+	name: "Calories",
+	unit: "kCal",
+	icon: "fas fa-fire",
+},
+{
+	name: "Protein",
+	unit: "g",
+	icon: "fas fa-chicken",
+},
+{
+	name: "Carbohydrates",
+	unit: "g",
+	icon: "fas fa-apple",
+},
+{
+	name: "Lipids",
+	unit: "g",
+	icon: "fas fa-hambuer",
+}];
+
+const translate = (key) => {
+	return ({
+		"calorieCount": "Calories",
+		"proteinCount": "Protein",
+		"carbohydrateCount": "Carbohydrates",
+		"lipidCount": "Lipids",
+	})[key];
+};
+
 const KeyChart = ({ id }) => {
 
 	const { data, loading, error} = useFetch(`${id}`);
+
+	const renderKey = (keysData) => {
+		return (
+			Object.keys(keysData)
+				.map(key => ({
+					...UNITS.find(e => e.name === translate(key)),
+					value: keysData[key]
+				}))
+		);
+	};
+	
+	const renderList = (keyList) => {
+		console.log(keyList.map(e => e));
+		return (
+			keyList.map((elt, index) => {
+				return (
+					<li key={index}>{elt.name}</li>
+				);
+			})
+		);
+	};
 
 	return (
 		<>
@@ -19,7 +70,9 @@ const KeyChart = ({ id }) => {
 
 			{data &&
 				<ResponsiveContainer width="100%" height="100%">
-					<span>{JSON.stringify(data.id)}</span>
+					<ul>
+						{renderList(renderKey(data.keyData))}
+					</ul>
 				</ResponsiveContainer>
 			}
 		</>
