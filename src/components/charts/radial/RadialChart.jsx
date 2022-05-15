82fc42b1befcc3c07cac1213a78cb8e2a35dfc3b
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-15 23:49:13 +0200

    working radial chart

--- src/components/charts/radial/RadialChart.jsx
+++ src/components/charts/radial/RadialChart.jsx
@@ -3,6 +3,7 @@ import {
 	ResponsiveContainer,
 	RadialBarChart,
 	RadialBar,
+	PolarAngleAxis
 } from "recharts";
 
 import PropTypes from "prop-types";
@@ -18,21 +19,34 @@ const RadialChart = ({ data }) => {
 				<RadialBarChart
 					data={[
 						{score: (score(data)*100)},
-						{score: 100}
 					]}
 					startAngle={90}
 					endAngle={450}
-					innerRadius="70%"
+					innerRadius="90%"
 					outerRadius="100%"
 				>
+					<text 
+						x={10}
+						y={30}
+						fill="black"
+						fontWeight="bolder"
+					>Score</text>
 					<text fill="black" textAnchor="middle" dominantBaseline="central">
 						<tspan x="50%" y="45%" fontSize="28" fontWeight="bolder">{`${score(data) * 100}%`}</tspan>
 						<tspan x="50%" y="56%" fill="grey" fontSize="14">{"de votre"}</tspan>
 						<tspan x="50%" y="63%" fill="grey" fontSize="14">{"objectif"}</tspan>
 					</text>
+					<PolarAngleAxis
+						type="number"
+						domain={[0, 100]}
+						dataKey={"value"}
+						angleAxisId={0}
+						tick={false}
+					/>
 					<RadialBar 
 						dataKey="score"
-						fill="var(--clr-primary)">
+						fill="var(--clr-primary)"
+					>
 					</RadialBar>
 				</RadialBarChart>
 			</ResponsiveContainer>
